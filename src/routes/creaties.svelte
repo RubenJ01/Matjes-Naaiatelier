3dfb6f976012f2070e95041c1be26ae1642aadd9
Lieuwe Baron <baronlieuwe38@gmail.com>
2022-07-08 00:39:48 +0200

    Update creaties.svelte

--- src/routes/creaties.svelte
+++ src/routes/creaties.svelte
@@ -1,7 +1,7 @@
 <script>
 	let contentBlocks = [
 		{
-            id: '1',
+			id: '1',
 			src: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
 			name: 'boom',
 			price: '€20.00,-',
@@ -9,7 +9,7 @@
 				'tSed tempor turpis et neque dictum finibus. Suspendisse gravida risus ut urna euismod suscipit.est1'
 		},
 		{
-            id: '2',
+			id: '2',
 			src: 'https://cdn.pixabay.com/photo/2013/04/04/12/34/mountains-100367_960_720.jpg',
 			name: 'vogels in de lucht',
 			price: '€10.00,-',
@@ -17,7 +17,15 @@
 				'Sed tempor turpis et neque dictum finibus. Suspendisse gravida risus ut urna euismod suscipi Sed tempor turpis et neque dictum finibus. Suspendisse gravida risus ut urna euismod suscipit.Sed tempor turpis et neque dictum finibus.  Suspendisse gravida risus ut urna euismod suscipit.'
 		},
 		{
-            id: '3',
+			id: '3',
+			src: 'https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_960_720.jpg',
+			name: 'meer landschap',
+			price: '€30.00,-',
+			description:
+				'Sed tempor turpis et neque dictum finibus. Suspendisse gravida risus ut urna euismod suscipit.'
+		},
+		{
+			id: '4',
 			src: 'https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_960_720.jpg',
 			name: 'meer landschap',
 			price: '€30.00,-',
@@ -26,30 +34,47 @@
 		}
 	];
 
-    function handleClick(id) {
-        alert(id)
-    }
+	function handleClick(id) {
+		alert(id);
+	}
 </script>
 
 <main>
-	{#each contentBlocks as product}
-		<div id={product.id} class="creationContainer" on:click={() => {handleClick(product.id)}}>
-			<div class="halfContainerText">
-				<h3>{product.name}</h3>
-				<h5>{product.price}</h5>
+	<div class="fullContainer">
+		{#each contentBlocks as product}
+			<div
+				id={product.id}
+				class="creationContainer"
+				on:click={() => {
+					handleClick(product.id);
+				}}
+			>
+				<div class="halfContainerText">
+					<h3>{product.name}</h3>
+					<h5>{product.price}</h5>
+				</div>
+				<div class="halfContainerImg">
+					<img src={product.src} alt={product.name} />
+				</div>
 			</div>
-            <div class="halfContainerImg">
-				<img src={product.src} alt={product.name}/>
-			</div>
-		</div>
-	{/each}
+		{/each}
+	</div>
 </main>
 
 <style>
+	.fullContainer {
+		margin: auto;
+		width: 75%;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+	}
+
 	.creationContainer {
+		flex: 50%;
 		margin: auto;
 		max-width: 25vw;
-        min-height: 25vh;
+		min-height: 25vh;
 		margin-top: 10vh;
 		border: 1px solid #bbb;
 		text-align: center;
@@ -59,7 +84,7 @@
 		float: bottom;
 		display: flex;
 		justify-content: center;
-        height: auto;
+		height: auto;
 		width: 100%;
 	}
 
@@ -77,9 +102,16 @@
 	}
 
 	img {
-        align-self: center;
-        width: 100%;
-        object-fit: cover;
+		align-self: center;
+		width: 100%;
+		object-fit: cover;
 		padding: 0;
+	}
+
+	@media screen and (max-width: 1000px) {
+		.creationContainer {
+			flex: 100%;
+            max-width: 50vw;
+		}
 	}
 </style>
