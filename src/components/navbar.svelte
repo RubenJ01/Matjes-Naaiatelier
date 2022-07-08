8623eddc16d86ed5be98c5cb27b3941e653ee2d3
Lieuwe Baron <baronlieuwe38@gmail.com>
2022-07-09 01:19:36 +0200

    finished reworking navbar in tailwind

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,35 +1,37 @@
 <nav class="bg-green-300">
-  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-    <div class="relative flex items-center justify-between h-16">
+  
+	<div class="flex items-center justify-between h-16 hidden sm:inline">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
         
       </div>
-      <div class="flex-1 flex items-left justify-left sm:items-stretch sm:justify-start">
+      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
         <div class="hidden sm:block sm:ml-6">
           <div class="flex space-x-8">
-            <a href="/" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-2 rounded-md text-2xl font-large">Home</a>
+            <a href="/" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-5 rounded-md text-2xl font-large">Home</a>
 
-            <a href="/creaties" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-2 rounded-md text-2xl font-large">Creaties</a>
+            <a href="/creaties" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-5 rounded-md text-2xl font-large">Creaties</a>
 
-            <a href="/about" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-2 rounded-md text-2xl font-large">About</a>
+            <a href="/about" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-5 rounded-md text-2xl font-large">About</a>
 
-            <a href="/blog" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-2 rounded-md text-2xl font-large">Blog</a>
+            <a href="/blog" class="text-gray-800 hover:bg-white hover:text-gray-800 px-3 py-5 rounded-md text-2xl font-large">Blog</a>
           </div>
         </div>
       </div>
-    </div>
-  </div>
+	</div>
+ 
+
+
 
   <!-- Mobile menu, show/hide based on menu state. -->
   <div class="sm:hidden" id="mobile-menu">
     <div class="px-2 pt-2 pb-3 space-y-1">
-      <a href="/" class="text-gray-800 hover:bg-white hover:text-gray-800 block px-3 py-2 rounded-md text-base font-medium">Home</a>
+      <a href="/" class="text-gray-800 hover:bg-white hover:text-gray-800 block px-3 py-2 rounded-md text-xl font-large">Home</a>
 
-      <a href="/creaties" class="text-gray-800 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Creaties</a>
+      <a href="/creaties" class="text-gray-800 hover:bg-white hover:text-gray-800 block px-3 py-2 rounded-md text-xl font-large">Creaties</a>
 
-      <a href="about" class="text-gray-800 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+      <a href="about" class="text-gray-800 hover:bg-white hover:text-gray-800 block px-3 py-2 rounded-md text-xl font-large">About</a>
 
-      <a href="/blog" class="text-gray-800 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Blog</a>
+      <a href="/blog" class="text-gray-800 hover:bg-white hover:text-gray-800 block px-3 py-2 rounded-md text-xl font-large">Blog</a>
     </div>
   </div>
 </nav>
