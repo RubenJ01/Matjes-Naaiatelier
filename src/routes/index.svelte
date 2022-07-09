ee5337bbc8005be784e1a8746ef85b983a876ed5
Lieuwe Baron <baronlieuwe38@gmail.com>
2022-07-10 01:02:58 +0200

    small css changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <main>
-	<h1 class="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">Tailwindggg Elements</h1>
+	<h1 class="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600 text-center">Home</h1>
 </main>
 
 <style>
