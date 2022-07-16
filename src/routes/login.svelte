24415cfd05d2e755763f59c49f59822520b78d1b
Lieuwe Baron <baronlieuwe38@gmail.com>
2022-07-16 17:04:18 +0200

    improved css

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -3,7 +3,8 @@
 
 <main>
 	<div class="lg:w-1/2 w-10/12 border rounded mx-auto my-24 flex flex-col">
-		<h1 class="text-center self-center text-2xl w-full py-5">Login</h1>
+		<h1 class="text-center self-center text-4xl w-full py-5">Login</h1>
+        <hr class="border rounded w-10/12 self-center">
 		<input
 			type="text"
 			placeholder="gebruikersnaam"
@@ -23,7 +24,7 @@
 			class="my-5 mb-5 py-4 mx-auto self-center text-center text-center bg-white border border focus:outline-none focus:border-green-300 focus:ring-green-300 block w-10/12 rounded-md  text-sm lg:text-lg focus:ring-1"
 		/>
 
-        <button class="bg-white hover:border-green-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 w-fill rounded self-center mb-10 text-sm lg:text-lg" >Log In</button>
+        <button class="bg-white hover:border-green-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 w-fill rounded self-center mb-5 text-sm lg:text-lg" >Log In</button>
 	</div>
 </main>
 
