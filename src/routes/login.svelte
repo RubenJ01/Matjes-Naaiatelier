ec2dfe2187776fd65e230a56170eeb0f8af61af5
Lieuwe Baron <baronlieuwe38@gmail.com>
2022-07-16 17:05:38 +0200

    added id to login button

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -24,7 +24,7 @@
 			class="my-5 mb-5 py-4 mx-auto self-center text-center text-center bg-white border border focus:outline-none focus:border-green-300 focus:ring-green-300 block w-10/12 rounded-md  text-sm lg:text-lg focus:ring-1"
 		/>
 
-        <button class="bg-white hover:border-green-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 w-fill rounded self-center mb-5 text-sm lg:text-lg" >Log In</button>
+        <button class="bg-white hover:border-green-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 w-fill rounded self-center mb-5 text-sm lg:text-lg" id="loginButton">Log In</button>
 	</div>
 </main>
 
